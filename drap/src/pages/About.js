4d25471f23e233477b6766e34706aeac7eec14f0
Elliot GM <elliot.giron-michel@epitech.eu>
2021-08-11 13:21:44 +0200

    [...]

---
+++ drap/src/pages/About.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <Logo />
+            <h1>About</h1>
+            <br/>
+            <p>lorem zehytrezefrghgfe Illud tamen clausos vehementer angebat quod captis navigiis,
+                quae frumenta vehebant per flumen, Isauri quidem alimentorum copiis adfluebant, ipsi
+                vero solitarum rerum cibos iam consumendo inediae propinquantis aerumnas exitialis horrebant.
+                Accedebant enim eius asperitati, ubi inminuta vel laesa amplitudo imperii dicebatur, et
+                iracundae suspicionum quantitati proximorum cruentae blanditiae exaggerantium incidentia et
+                dolere inpendio simulantium, si principis periclitetur vita, a cuius salute velut filo
+                pendere statum orbis terrarum fictis vocibus exclamabant.</p>
+            <br/>
+            <p>lorem zehytrezefrghgfe Illud tamen clausos vehementer angebat quod captis navigiis,
+                quae frumenta vehebant per flumen, Isauri quidem alimentorum copiis adfluebant, ipsi
+                vero solitarum rerum cibos iam consumendo inediae propinquantis aerumnas exitialis horrebant.
+                Accedebant enim eius asperitati, ubi inminuta vel laesa amplitudo imperii dicebatur, et
+                iracundae suspicionum quantitati proximorum cruentae blanditiae exaggerantium incidentia et
+                dolere inpendio simulantium, si principis periclitetur vita, a cuius salute velut filo
+                pendere statum orbis terrarum fictis vocibus exclamabant.
+                Ex turba vero imae sortis et paupertinae in tabernis aliqui pernoctant vinariis, non nulli
+                velariis umbraculorum theatralium latent, quae Campanam imitatus lasciviam Catulus in aedilitate
+                sua suspendit omnium primus; aut pugnaciter aleis certant turpi sono fragosis naribus introrsum reducto
+                spiritu concrepantes; aut quod est studiorum omnium maximum ab ortu lucis ad vesperam sole fatiscunt vel pluviis,
+                per minutias aurigarum equorumque praecipua vel delicta scrutantes.</p>
+        </div>
+    );
+};
+
+export default About;
